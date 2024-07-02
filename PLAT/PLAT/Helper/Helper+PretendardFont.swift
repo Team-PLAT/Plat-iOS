a89a1044ef2420e2165843f31bce8fabf3797bf4
Park Junwoo <ays99111@naver.com>
2024-07-02 20:11:19 +0900

    [CHORE/#10] Font 추가

---
+++ PLAT/PLAT/Helper/Helper+PretendardFont.swift
@@ -0,0 +1,31 @@
+//
+//  Helper+PretendardFont.swift
+//  PLAT
+//
+//  Created by 박준우 on 7/2/24.
+//
+
+import SwiftUI
+
+extension Font {
+    enum Head {
+        static let head1: Font = .custom("Pretendard-SemiBold", size: 28)
+        static let head2: Font = .custom("Pretendard-SemiBold", size: 24)
+        static let head3: Font = .custom("Pretendard-SemiBold", size: 22)
+        static let head4: Font = .custom("Pretendard-SemiBold", size: 20)
+        static let head5: Font = .custom("Pretendard-SemiBold", size: 18)
+    }
+    
+    enum Body {
+        static let body1: Font = .custom("Pretendard-Medium", size: 18)
+        static let body2: Font = .custom("Pretendard-SemiBold", size: 16)
+        static let body3: Font = .custom("Pretendard-Regular", size: 16)
+        static let body4: Font = .custom("Pretendard-SemiBold", size: 14)
+        static let body5: Font = .custom("Pretendard-Regular", size: 14)
+    }
+    
+    enum Caption {
+        static let caption1: Font = .custom("Pretendard-Regular", size: 12)
+        static let caption2: Font = .custom("Pretendard-Regular", size: 10)
+    }
+}
