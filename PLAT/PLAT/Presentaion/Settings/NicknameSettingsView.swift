24dff06ed9d6aa41881456148de69e9962f33f58
woboneyo <bluewing1004@gmail.com>
2024-07-07 15:24:24 +0900

    [FEAT/#11] PlatTextField 컴포넌트 생성

--- PLAT/PLAT/Presentaion/Settings/NicknameSettingsView.swift
+++ PLAT/PLAT/Presentaion/Settings/NicknameSettingsView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct NicknameSettingsView: View {
+    
+    @Environment(UserUseCase.self) private var userUseCase
+    @State private(set) var nicknameText: String
+    
     var body: some View {
-        Text("NicknameSettingsView")
+        VStack(alignment: .leading) {
+            Text("새로운 닉네임을 입력해주세요")
+                .font(.Head.head4)
+                .padding(.horizontal, 25)
+            
+            Spacer()
+                .frame(height: 12)
+            
+            PlatTextField(
+                text: $nicknameText,
+                placeholder: "닉네임을 입력해주세요.",
+                state: .normal
+            )
+            .padding(.horizontal, 18)
+        }
+        .navigationTitle("닉네임 변경")
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.platBackground)
     }
 }
 
 #Preview {
-    NicknameSettingsView()
+    NicknameSettingsView(nicknameText: "IPSUM_LOREM")
+        .environment(PreviewHelper.mockUserUseCase)
 }
