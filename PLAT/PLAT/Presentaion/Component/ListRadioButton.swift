294a2daa8600bd0d169cb375c7077366cb3436c2
woboneyo <bluewing1004@gmail.com>
2024-07-07 17:31:52 +0900

    [FEAT/#11] ListRadioButton 컴포넌트 구현

---
+++ PLAT/PLAT/Presentaion/Component/ListRadioButton.swift
@@ -0,0 +1,97 @@
+//
+//  ListRadioButton.swift
+//  PLAT
+//
+//  Created by 조우현 on 7/7/24.
+//
+
+import SwiftUI
+
+struct ListRadioButton: View {
+    
+    enum State {
+        case radio
+        case none
+    }
+    
+    let state: State
+    let title: String
+    let content: String
+    let icon: ImageResource
+    var isSelected: Bool = false
+    var tapAction: () -> Void
+    
+    var body: some View {
+        HStack {
+            Image(icon)
+                .padding(.trailing, 10)
+            ButtonTextView(title: title, content: content)
+            Spacer()
+            if state == .radio {
+                RadioCircle(isSelected: isSelected)
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .frame(height: 64)
+        .overlay {
+            RoundedRectangle(cornerRadius: 8)
+                .strokeBorder(isSelected ? .platPurple : .platBackground, lineWidth: 2)
+        }
+        .onTapGesture {
+            tapAction()
+        }
+    }
+}
+
+// MARK: - ButtonTextView
+
+private struct ButtonTextView: View {
+    
+    let title: String
+    let content: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.Body.body2)
+                .foregroundStyle(.white)
+            
+            Text(content)
+                .font(.Caption.caption1)
+                .foregroundStyle(.gray7)
+        }
+    }
+}
+
+// MARK: - RadioCircle
+
+private struct RadioCircle: View {
+    
+    var isSelected: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 18, height: 18)
+                .foregroundStyle(.gray6)
+            
+            Circle()
+                .frame(width: 16, height: 16)
+                .foregroundStyle(.platBackground)
+            
+            Circle()
+                .frame(width: 10, height: 10)
+                .foregroundStyle(.platPurple)
+                .opacity(isSelected ? 1 : 0)
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        ListRadioButton(state: .radio, title: "Apple Music 연결됨", content: "Apple Music 스트리밍 계정과 연결되어 있어요", icon: .appleMusic, isSelected: true) {}
+        ListRadioButton(state: .radio, title: "Apple Music 연결됨", content: "Apple Music 스트리밍 계정과 연결되어 있어요", icon: .appleMusic) {}
+        ListRadioButton(state: .none, title: "Apple Music 연결됨", content: "Apple Music 스트리밍 계정과 연결되어 있어요", icon: .appleMusic) {}
+    }
+}
