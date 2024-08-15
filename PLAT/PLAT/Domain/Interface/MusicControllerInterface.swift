3c21f659959f08ea688c844797f5fb29ba228516
thinkyside <eunlyuing@gmail.com>
2024-08-15 15:46:19 +0900

    [FEAT/#32] MusicControllerInterface 생성

---
+++ PLAT/PLAT/Domain/Interface/MusicControllerInterface.swift
@@ -0,0 +1,16 @@
+//
+//  MusicControllerInterface.swift
+//  PLAT
+//
+//  Created by 김민준 on 8/15/24.
+//
+
+import Foundation
+
+protocol MusicControllerInterface {
+    func play()
+    func pause()
+    func previous()
+    func next()
+    func repeatPlayback()
+}
---
+++ PLAT/PLAT/Presentaion/TrackDetail/TrackDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackDetailView.swift
+//  PLAT
+//
+//  Created by 김민준 on 8/15/24.
+//
+
+import SwiftUI
+
+struct TrackDetailView: View {
+    var body: some View {
+        Text("TrackDetailView")
+    }
+}
+
+#Preview {
+    TrackDetailView()
+}
