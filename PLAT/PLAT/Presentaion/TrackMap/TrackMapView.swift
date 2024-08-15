597fdb1a9295845fd8bead361dbc7dde63557ebc
woboneyo <bluewing1004@gmail.com>
2024-08-15 18:12:46 +0900

    [Feat/#31] TrackMapView에 Mock location 지정

--- PLAT/PLAT/Presentaion/TrackMap/TrackMapView.swift
+++ PLAT/PLAT/Presentaion/TrackMap/TrackMapView.swift
@@ -10,9 +10,10 @@ import MapKit
 
 struct TrackMapView: View {
     @Environment(TrackMapUseCase.self) private var trackMapUseCase: TrackMapUseCase
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: MockDataBuilder.location.latitude, longitude: MockDataBuilder.location.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
     
     var body: some View {
-        Map()
+        Map(position: $position)
     }
 }
 
