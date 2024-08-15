f181f5a15f79c2ea8f74093fb8028292b64fa491
thinkyside <eunlyuing@gmail.com>
2024-08-15 15:58:16 +0900

    [FEAT/#32] TrackInterface 구현

---
+++ PLAT/PLAT/Domain/Interface/TrackInterface.swift
@@ -0,0 +1,15 @@
+//
+//  TrackInterface.swift
+//  PLAT
+//
+//  Created by 김민준 on 8/15/24.
+//
+
+import Foundation
+
+protocol TrackInterface {
+    func like(trackId: String)
+    func delete(trackId: String)
+    func report(trackId: String)
+    func addToPlaylist(trackId: String, playlistId: String)
+}
