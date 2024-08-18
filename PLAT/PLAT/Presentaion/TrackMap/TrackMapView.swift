0cd8b961b3b79389204bc1fcc126cd8f9e6bddde
woboneyo <bluewing1004@gmail.com>
2024-08-18 16:15:35 +0900

    [Feat/#31] TrackMapView에서 사용자가 지도를 이동하지 못하게 설

--- PLAT/PLAT/Presentaion/TrackMap/TrackMapView.swift
+++ PLAT/PLAT/Presentaion/TrackMap/TrackMapView.swift
@@ -18,7 +18,7 @@ struct TrackMapView: View {
     
     var body: some View {
         ZStack(alignment: .topLeading) {
-            Map(position: $position) {
+            Map(position: $position, interactionModes: []) {
                 Annotation("", coordinate: CLLocationCoordinate2D(latitude: currentCoordinate.latitude, longitude: currentCoordinate.longitude)) {
                     CurrentLocationDotView()
                 }
@@ -29,6 +29,7 @@ struct TrackMapView: View {
                             .onTapGesture {
                                 selectedTrack = track
                                 showTrackDetail.toggle()
+                                print(track)
                                 // TODO: 해당 트랙의 정보를 담고있는 TrackDetailView로 이동
                             }
                     }
