5b9d95b980d4de9cdfc16c1f149176e38b0d2cd7
thinkyside <eunlyuing@gmail.com>
2024-08-18 16:46:13 +0900

    [FEAT/#32] App 파일 초기화

--- PLAT/PLAT/App/PLATApp.swift
+++ PLAT/PLAT/App/PLATApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct PLATApp: App {
     var body: some Scene {
         WindowGroup {
-            // OnboardingView()
-            TrackDetailView()
+            OnboardingView()
         }
     }
 }
