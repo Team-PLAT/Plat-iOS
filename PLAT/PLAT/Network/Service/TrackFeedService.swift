6d1d46eb2463c40354cf34ed88a085c396af0627
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-08-20 10:43:26 +0900

    [FEAT/#52] 네트워크 매니저 수정

--- PLAT/PLAT/Network/Service/TrackFeedService.swift
+++ PLAT/PLAT/Network/Service/TrackFeedService.swift
@@ -14,39 +14,24 @@ class FeedService {
     func searchTrackFeed() async {
         let client = NetworkClient()
         let authToken = "tokenishere"
-        do {
-            let url = URL(string: Config.baseURL + "/tracks/feeds")!
-            let response: BaseResponse<TrackFeedResponse> = try await client.get(url: url, authToken: authToken)
-            print(response)
-            // 우리가 구현한 모델에 연결
-        } catch {
-            print (error.localizedDescription)
-        }
+        let url = URL(string: Config.baseURL + "/tracks/feeds")!
+        let response: Result<BaseResponse<TrackDetailResponse>, Error> = await client.get(url: url, authToken: authToken)
+        print(response)
     }
     
     func searchTrackDetail(trackId: Int64) async {
         let client = NetworkClient()
         let authToken = "tokenishere"
-        do {
-            let url = URL(string: "\(Config.baseURL)/tracks/\(trackId)")!
-            let response: BaseResponse<TrackFeedResponse> = try await client.get(url: url, authToken: authToken)
-            print(response)
-            // 우리가 구현한 모델에 연결
-        } catch {
-            print(error.localizedDescription)
-        }
+        let url = URL(string: "\(Config.baseURL)/tracks/\(trackId)")!
+        let response: Result<BaseResponse<TrackDetailResponse>, Error> = await client.get(url: url, authToken: authToken)
+        print(response)
     }
     
-    func postTrackFeed(trackDetail: TrackDetailDto) async {
+    func postTrackFeed(trackDetail: TrackDetailRequest) async {
         let client = NetworkClient()
         let authToken = "tokenishere"
-        do {
-            let url = URL(string: "\(Config.baseURL)/tracks")!
-            let response: BaseResponse<TrackFeedResponse> = try await client.post(url: url, body: trackDetail, authToken: authToken)
-            print(response)
-            // 우리가 구현한 모델에 연결
-        } catch {
-            print(error.localizedDescription)
-        }
+        let url = URL(string: "\(Config.baseURL)/tracks")!
+        let response: Result<BaseResponse<TrackDetailResponse>, Error>  = await client.post(url: url, body: trackDetail, authToken: authToken)
+        print(response)
     }
 }
