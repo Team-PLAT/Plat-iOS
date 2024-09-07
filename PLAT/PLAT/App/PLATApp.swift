4877ca95fb7c10d44f3d9d70184a4be06b0e06f3
thinkyside <eunlyuing@gmail.com>
2024-09-07 16:49:11 +0900

    [FEAT/#71] NetworkLog 객체 생성 및 get 메서드 내 적용

--- PLAT/PLAT/App/PLATApp.swift
+++ PLAT/PLAT/App/PLATApp.swift
@@ -19,10 +19,24 @@ struct PLATApp: App {
         userSessionService: StubUserSessionService() // TODO: Stub 교체
     )
     
+    struct ProfileResponse: Decodable {
+        let memberId: Int64
+        let nickname: String
+        let avatar: String
+    }
+    
     var body: some Scene {
         WindowGroup {
             if !authUseCase.state.isLoginComplete {
                 OnboardingView()
+                    .onAppear {
+                        Task {
+                            let response: Result<BaseResponse<ProfileResponse>, Error> =  await NetworkClient().get(
+                                url: APIs.Plat.Members.fetchProfile.url,
+                                authToken: "eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJhY2Nlc3MiLCJtZW1iZXJJZCI6MSwiY2xpZW50SWQiOiJzdHJpbmciLCJwZXJtaXNzaW9uUm9sZSI6IkFETUlOIiwiaWF0IjoxNzI1NjkxMjEyLCJleHAiOjE3MjY1NTUyMTJ9.JSz5TxZibMqusdoPiG29-z8kXJXALj_OZ_sQ-0mUJ8pIsgNMby0eVXZIAcvI5g5QMqC56fPkhmKRCMJjtnLt4A"
+                            )
+                        }
+                    }
             } else {
                 MainView()
             }
--- PLAT/PLAT/Network/NetworkManager.swift
+++ PLAT/PLAT/Network/NetworkManager.swift
@@ -27,17 +27,50 @@ class NetworkClient: APIMethod {
             
             let (data, response) = try await URLSession.shared.data(for: request)
             
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                return .failure(NetworkError.serverError(statusCode: (response as? HTTPURLResponse)?.statusCode ?? 0))
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return .failure(NetworkError.httpResponseError)
             }
             
-            let decodedData = try JSONDecoder().decode(T.self, from: data)
-            return .success(decodedData)
+            guard (200...299).contains(httpResponse.statusCode) else {
+                let error = NetworkError.serverError(statusCode: httpResponse.statusCode)
+                NetworkLog.failure(
+                    url: url,
+                    statusCode: httpResponse.statusCode,
+                    error: error
+                )
+                return .failure(error)
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                NetworkLog.success(
+                    url: url,
+                    statusCode: httpResponse.statusCode,
+                    data: decodedData
+                )
+                return .success(decodedData)
+            } catch {
+                NetworkLog.failure(
+                    url: url,
+                    statusCode: httpResponse.statusCode,
+                    error: error
+                )
+                return .failure(NetworkError.decodingError)
+            }
         } catch {
             if let urlError = error as? URLError {
+                NetworkLog.failure(
+                    url: url,
+                    statusCode: 000,
+                    error: error
+                )
                 return .failure(NetworkError.urlError(urlError))
             } else {
+                NetworkLog.failure(
+                    url: url,
+                    statusCode: 000,
+                    error: error
+                )
                 return .failure(NetworkError.error(error))
             }
         }
