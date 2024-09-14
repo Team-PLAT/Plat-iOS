244fbb7578b7b310f19084290b358ba565f13775
Park Junwoo <ays99111@naver.com>
2024-09-14 19:41:06 +0900

    [FEAT/#86] TrackAppendSearchView 음악 리스트 행간 패딩 조절

--- PLAT/PLAT/Presentaion/TrackAppend/TrackAppendSearchView.swift
+++ PLAT/PLAT/Presentaion/TrackAppend/TrackAppendSearchView.swift
@@ -151,6 +151,7 @@ private struct TrackAppendMusicListView: View {
                         selectedMusic = music.wrappedValue
                     }
             }
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
             .listRowSeparator(.hidden)
             .listRowBackground(Color.clear)
         }
