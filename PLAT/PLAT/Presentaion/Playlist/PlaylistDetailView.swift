f44560e76d0ebae0778cd24f191d5cef9be0c945
Park Junwoo <ays99111@naver.com>
2024-09-17 00:05:44 +0900

    [FEAT/#97] PlaylistDetailView 파일 생성

---
+++ PLAT/PLAT/Presentaion/Playlist/PlaylistDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistDetailView.swift
+//  PLAT
+//
+//  Created by 박준우 on 9/17/24.
+//
+
+import SwiftUI
+
+struct PlaylistDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaylistDetailView()
+}
