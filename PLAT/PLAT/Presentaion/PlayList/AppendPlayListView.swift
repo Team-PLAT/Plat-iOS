5856eb9af6729199c85f80a085a67273a4d83719
Park Junwoo <ays99111@naver.com>
2024-09-16 20:51:26 +0900

    [FEAT/#95] AppendPlayListView 네비게이션 바 구현

--- PLAT/PLAT/Presentaion/PlayList/AppendPlayListView.swift
+++ PLAT/PLAT/Presentaion/PlayList/AppendPlayListView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct AppendPlayListView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Hello, World!")
+                .navigationTitle("새로운 플레이리스트")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            HStack(spacing: 3) {
+                                Image(systemName: "chevron.backward")
+                                Text("취소")
+                                    .font(.Body.body2)
+                            }
+                            .foregroundStyle(.platPurple)
+                        }
+                        
+                    }
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            // TODO: 플레이 생성하기 기능 추가
+                        } label: {
+                            Text("생성")
+                                .foregroundStyle(.platPurple)
+                                .font(.Body.body2)
+                        }
+                    }
+                }
+        }
     }
 }
 
