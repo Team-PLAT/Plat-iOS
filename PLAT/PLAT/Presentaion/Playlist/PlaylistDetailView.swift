50bd3ee5da7762470b40a520f8d3ee1ee58209fe
Park Junwoo <ays99111@naver.com>
2024-09-17 02:46:16 +0900

    [FEAT/#97] PlaylistDetailView Playlist 데이터 주입 받을 @Binding 변수 생성

--- PLAT/PLAT/Presentaion/Playlist/PlaylistDetailView.swift
+++ PLAT/PLAT/Presentaion/Playlist/PlaylistDetailView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct PlaylistDetailView: View {
+    
+    @Binding private(set) var selectedPlaylist: Playlist
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .navigationBarBackButtonHidden(true)
