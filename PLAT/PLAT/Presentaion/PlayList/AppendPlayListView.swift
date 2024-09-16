840c1c1bc539983f138dca04bcd4f9e7b4c5a591
Park Junwoo <ays99111@naver.com>
2024-09-16 20:06:20 +0900

    [FEAT/#95] AppendPlayListView 파일 생성

---
+++ PLAT/PLAT/Presentaion/PlayList/AppendPlayListView.swift
@@ -0,0 +1,18 @@
+//
+//  AppendPlayListView.swift
+//  PLAT
+//
+//  Created by 박준우 on 9/16/24.
+//
+
+import SwiftUI
+
+struct AppendPlayListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppendPlayListView()
+}
